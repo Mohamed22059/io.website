3128ce8f92cc37e451eee81c8ca908e32152826b
Mohamed Mustafa <145797690+Mohamed22059@users.noreply.github.com>
2024-10-03 11:33:35 +0300

    Add files via upload

---
+++ js/work5.js
@@ -0,0 +1,10 @@
+// let link = document.querySelector('.header .links')
+// let ul = document.querySelector('.header .links ul')
+
+
+// ul.classList.add('remove-js')
+
+// link.addEventListener('click',function(){
+//     ul.classList.toggle('remove-js')
+
+// })
